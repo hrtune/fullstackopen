3d5b22a15c0fc151f7a6925b840049def4a774a5
Haruta Tasaki <hrtune@gmail.com>
2022-12-06 20:42:36 +0900

    Update Apps for course information part2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,32 @@
+const Header = (props) => (
+  <>
+    <h1>{props.course}</h1>
+  </>
+)
+
+const Part = (props) => (
+  <>
+    <p>{props.title} {props.point}</p>
+  </>
+)
+
+const Content = (props) => (
+  <div>
+    <Part title={props.p1} point={props.ex1} />
+    <Part title={props.p2} point={props.ex2} />
+    <Part title={props.p3} point={props.ex3} />
+  </div>
+)
+const Total = (props) => {
+  const total = props.ex1 + props.ex2 + props.ex3
+  console.log("total is ", total)
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+}
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -9,17 +38,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content p1={part1} ex1={exercises3} p2={part2} ex2={exercises2} p3={part3} ex3={exercises3}/>
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
     </div>
   )
 }
