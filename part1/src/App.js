b0e4619ac811cc7925d1eb9b3885abd5820bc919
Haruta Tasaki <hrtune@gmail.com>
2022-12-06 19:19:23 +0900

    Update App component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,16 +1,21 @@
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
-const App = () => (
-  <div>
-    <h1>Greetings</h1>
-    <Hello />
-  </div>
-)
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26+10}/>
+      <Hello name={name} age={age}/>
+    </div>
+  )
+}
 
 export default App
