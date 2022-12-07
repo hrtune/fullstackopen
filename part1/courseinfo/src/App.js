8cb4b376abae335c22f6fb4ff8c76bb3e65f6c15
Haruta Tasaki <hrtune@gmail.com>
2022-12-07 19:49:51 +0900

    Update App component for step5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,6 @@
 const Header = (props) => (
   <>
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   </>
 )
 
@@ -33,27 +33,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  }
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  }
-  const part3 = {
-    name: "State of a component",
-    exercises: 14
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
   }
 
-  const parts = [part1, part2, part3]
-  
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
