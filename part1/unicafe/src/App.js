d9a910eb800814c44e4d63e7a59b8c44a641deff
Haruta Tasaki <hrtune@gmail.com>
2022-12-08 22:54:26 +0900

    Create App for unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Header = ({text}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1>{text}</h1>
+    </>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const ShowStats = ({stats}) => (
+  <div>
+    <p>good {stats.good}</p>
+    <p>neutral {stats.neutral}</p>
+    <p>bad {stats.bad}</p>
+  </div>
+)
+
+const App = () => {
+
+  const [stats, setStats] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
+
+  const handleGood = () => setStats({...stats, good: stats.good + 1})
+  const handleNeutral = () => setStats({...stats, neutral: stats.neutral + 1})
+  const handleBad = () => setStats({...stats, bad: stats.bad + 1})
+
+  return (
+    <div>
+      <Header text={"give feedback"} />
+      <Button onClick={handleGood} text={"good"} />
+      <Button onClick={handleNeutral} text={"neutral"} />
+      <Button onClick={handleBad} text={"bad"} />
+      <Header text={"statistics"} />
+      <ShowStats stats={stats} />
     </div>
   );
 }
