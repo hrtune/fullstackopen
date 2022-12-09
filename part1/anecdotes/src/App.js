c5b3a79497cdae5d68bbb374916dc4262adea3db
Haruta Tasaki <hrtune@gmail.com>
2022-12-09 20:14:14 +0900

    Update App for anecdotes step2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
+const Button = ({text, onClick}) => (
+  <button onClick={onClick}>{text}</button>
+)
+
+const App = () => {
+  
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients."
+  ];
+
+  const random = () => {
+    const max = anecdotes.length
+    return Math.trunc(Math.random() * max)
+  }
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoint] = useState(Array(anecdotes.length).fill(0))
+
+  const selectAnecdote = () => setSelected(random())
+  const voteToAnecdote = () => {
+    const copyPoints = [...points]
+    copyPoints[selected] += 1
+    setPoint(copyPoints)
+  }
+
+  console.log(selected)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>{anecdotes[selected]}</h4>
+      <p>has {points[selected]} votes</p>
+      <Button text="vote" onClick={voteToAnecdote} />
+      <Button text="next anecdote" onClick={selectAnecdote} />
     </div>
   );
 }
