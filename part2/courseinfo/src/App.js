0c867585e56fedac8127505a7ffa5d6a9ac29375
Haruta Tasaki <hrtune@gmail.com>
2022-12-10 19:48:54 +0900

    Separate Course from App as module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,31 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-
-const Total = ({ sum }) => <p><strong>Number of exercises {sum}</strong></p>;
-
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => <Part key={part.id} part={part} />)}
-  </>
-);
-
-const Course = ({course}) => {
-  const sum = course.parts.reduce((a, b) => a + b.exercises, 0)
-  return (
-  <>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total sum={sum} />
-  </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,34 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => (
+  <p>
+    <strong>Number of exercises {sum}</strong>
+  </p>
+);
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+
+const Course = ({ course }) => {
+  const sum = course.parts.reduce((a, b) => a + b.exercises, 0);
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={sum} />
+    </>
+  );
+};
+
+export default Course
