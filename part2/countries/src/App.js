c4f5c3e684138778b2fde7ad91dfb774d66e8352
Haruta Tasaki <hrtune@gmail.com>
2022-12-13 23:11:00 +0900

    Update countries for step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
 
+
 const ListOfCountries = ({countries}) => {
 
   const [ countryToShow, setCountryToShow ] = useState(null)
@@ -33,7 +34,7 @@ const ListOfCountries = ({countries}) => {
 const CountryHeader = ({text}) => {
   return (
     <div>
-      <h2>{text}</h2>
+      <h1>{text}</h1>
     </div>
   )
 }
@@ -82,6 +83,69 @@ const BasicData = ({country}) => {
   )
 }
 
+const WeatherHeader = ({city}) => (
+  <>
+    <h2>Weather in {city}</h2>
+  </>
+)
+
+const Temperature = ({temp}) => {
+  const K = 273.15
+  const temperature = Math.round((temp - K) * 100) / 100
+  return (
+    <>
+      <p>temperature {temperature} Celcius</p>
+    </>
+  )
+}
+
+const WeatherIcon = ({icon, description}) => (
+  <div>
+    <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt="few clouds" />
+  </div>
+)
+
+const Wind = ({wind}) => (
+  <div>
+    <p>wind {wind.speed} m/s</p>
+  </div>
+)
+
+const Weather = ({city}) => {
+
+  const [weatherData, setWeatherData] = useState(null)
+
+  useEffect(() => {
+    const api_key = process.env.REACT_APP_API_KEY
+    axios
+      .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${api_key}`
+      )
+      .then(
+        response => {
+          console.log(response.data.weather);
+          setWeatherData(response.data)
+        }
+      )
+  },[city])
+
+  if (!weatherData) {
+    return <></>
+  }
+
+  return (
+    <div>
+      <WeatherHeader city={city} />
+      <Temperature temp={weatherData.main.temp} />
+      <WeatherIcon
+        icon={weatherData.weather[0].icon}
+        description={weatherData.weather[0].description}
+      />
+      <Wind wind={weatherData.wind} />
+    </div>
+  );
+}
+
 const DetailedCountry = ({country}) => {
   return (
     <div>
@@ -89,6 +153,7 @@ const DetailedCountry = ({country}) => {
       <BasicData country={country} />
       <Languages languages={country.languages} />
       <Flag flag={country.flag} name={country.name}/>
+      <Weather city={country.capital}/>
     </div>
   )
 }
