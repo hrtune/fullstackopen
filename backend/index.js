64031b22e6b33df86a9f3464389822098eff92cb
Haruta Tasaki <hrtune@gmail.com>
2022-12-20 19:26:49 +0900

    Update backend to use mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const cors = require('cors')
+
+const Note = require('./models/note')
 
 app.use(express.static('build'))
 
@@ -55,17 +58,15 @@ app.get('/', (request, response) => {
 
 // http://localhost:3001/api/notes
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(n => n.id === id)
-    if (note) {
-      response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    Note.findById(request.params.id).then(note => {
+        response.json(note)
+    })
     
 })
 
@@ -111,16 +112,15 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId()
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.status(201).json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 
 })
 
@@ -130,6 +130,6 @@ const unknownEndPoint = (request, response) => {
 }
 app.use(unknownEndPoint)
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
