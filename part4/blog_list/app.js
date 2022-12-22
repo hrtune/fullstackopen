07cd0a678961f844fc846a00ba213cc2ae7a4f75
Haruta Tasaki <hrtune@gmail.com>
2022-12-22 21:58:54 +0900

    Refactor blog model

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -3,18 +3,9 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const Blog = require('./models/blog')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
