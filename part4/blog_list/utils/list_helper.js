abd8e3dd98d56ab785bd5e670a783d5e97202adc
Haruta Tasaki <hrtune@gmail.com>
2022-12-23 21:07:29 +0900

    Pass tests for step3

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -14,10 +14,33 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-    return blogs.reduce((a, blog) => a + blog.likes, 0)
+    return blogs.reduce((acc, blog) => acc + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((acc, blog) => {
+        if (acc === null || blog.likes > acc.likes) {
+            return blog
+        } else {
+            return acc
+        }
+    }, null)
+}
+
+/*
+return object:
+{
+  author: "Robert C. Martin",
+  blogs: 3
+}
+*/
+const mostBlogs = (blogs) => {
+    return null
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
