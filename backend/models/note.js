116d23ba6e1b7af27a3ab16666bb9ca85d011063
Haruta Tasaki <hrtune@gmail.com>
2022-12-28 22:23:26 +0900

    Update backend

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,29 +1,17 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
     minLength: 5,
     required: true
   },
-  date: {
-    type: Date,
-    required: true
-  },
+  type: Date,
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
 })
 
 noteSchema.set('toJSON', {
@@ -34,4 +22,6 @@ noteSchema.set('toJSON', {
   },
 })
 
-module.exports = mongoose.model('Note', noteSchema)
+const Note = mongoose.model('Note', noteSchema)
+
+module.exports = Note
