1af251f1b56f7fb33f08f2768f7d19445d5af0c5
Haruta Tasaki <hrtune@gmail.com>
2022-12-28 23:10:46 +0900

    Implement create_user for Ex 4.15

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 require('express-async-errors')
 
 mongoose.connect(config.MONGODB_URI)
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 
