100ab2727cfabbaa960bd1488d6dff8e7e56453d
Haruta Tasaki <hrtune@gmail.com>
2023-01-07 22:32:10 +0900

    Implement notifications (5.4)

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -2,15 +2,32 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import './App.css'
 
+const TIMEOUT = 2500
 
+const Notification = ({message, state}) => {
+  if (!message) {
+    return null
+  }
 
+  return (
+    <div className={state}>
+      {message}
+    </div>
+  )
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
 
 
   // load all blogs
@@ -27,7 +44,7 @@ const App = () => {
       const user = JSON.parse(loggedUserJSON)
       console.log(`user ${user.username} has logged in`);
       setUser(user)
-      //< set token
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -38,14 +55,15 @@ const App = () => {
     try {
       const user = await loginService.login({ username, password })
 
-      //< set token
+      blogService.setToken(user.token)
       window.localStorage.setItem('loggedBloglistUser', JSON.stringify(user))
       setUser(user)
       console.log(`user ${username} has logged in`);
       setUsername('')
       setPassword('')
     } catch (exception) {
-      //
+      setErrorMessage('username or password is incorrect')
+      setTimeout(() => setErrorMessage(''), TIMEOUT)
       console.log(exception.message);
       setPassword('')
     }
@@ -56,9 +74,78 @@ const App = () => {
     setUser(null)
   }
 
+  const handleCreate = async (event) => {
+    event.preventDefault()
+    try {
+      const newBlog = {
+        title,
+        author,
+        url,
+        likes: 0
+      }
+
+      const blog = await blogService.create(newBlog)
+
+      console.log(`blog ${blog.title} has created`)
+
+      setBlogs(blogs.concat(blog))
+
+      // clean up inputs on form
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+
+      setSuccessMessage(`a new blog ${blog.title} by ${blog.author} added`)
+      setTimeout(() => setSuccessMessage(''), TIMEOUT)
+
+    } catch (exception) {
+      setErrorMessage('something wrong with the new blog')
+      setTimeout(() => setErrorMessage(''), TIMEOUT)
+      console.log(exception.message);
+    }
+  }
+
+  const createForm = () => (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleCreate}>
+        <div>
+          title:
+          <input
+            type="text"
+            name="Title"
+            value={title}
+            onChange={({target}) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            name="Author"
+            value={author}
+            onChange={({target}) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            name="Url"
+            value={url}
+            onChange={({target}) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+
   const loginPage = () => (
     <div>
       <h2>log in to application</h2>
+      <Notification message={errorMessage} state="error" />
+      <Notification message={successMessage} state="success" />
       <form onSubmit={handleLogin}>
         <div>
           username
@@ -86,13 +173,15 @@ const App = () => {
   const blogPage = () => (
     <div>
       <h2>blogs</h2>
+      <Notification message={errorMessage} state="error" />
+      <Notification message={successMessage} state="success" />
       <p>
         {user.name} logged in<button onClick={handleLogout}>logout</button>
       </p>
+      {createForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
-      
     </div>
   )
 
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,13 +1,31 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = (newToken) => {
+  token = 'bearer ' + newToken
+}
+
 const getAll = async () => {
   const response = await axios.get(baseUrl)
   return response.data
 }
 
+const create = async (newBlog) => {
+  const config = {
+    headers: {
+      Authorization: token
+    }
+  }
+  const response = await axios.post(baseUrl, newBlog, config)
+  return response.data
+}
+
 const exports = {
-  getAll
+  getAll,
+  create,
+  setToken
 }
 
 export default exports
