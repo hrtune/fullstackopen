641017c80f0c9e7aa36be6b4e7a90d9231e81dc2
Haruta Tasaki <hrtune@gmail.com>
2023-01-10 20:14:50 +0900

    Implement detailed view of each blog (5.7)

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [viewMode, setViewMode] = useState(false)
+
+  const handleClick = () => {
+    setViewMode(!viewMode)
+  }
+
+  const simplifiedBlog = () => (
+    <div style={blogStyle}>
+      {blog.title} <em>by</em> {blog.author} <button onClick={handleClick}>view</button>
+    </div>
+
+  )
+
+  const detailedBlog = () => (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={handleClick}>hide</button> <br />
+      {blog.url} <br />
+      likes {blog.likes} <button>like</button> <br />
+      {blog.author}
+    </div>
+  )
+
+  return (
+    viewMode ? detailedBlog() : simplifiedBlog()
+  )
+}
 
 export default Blog
