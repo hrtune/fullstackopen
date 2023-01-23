57e361174bdce1944610dba03cddac989db0d508
Haruta Tasaki <hrtune@gmail.com>
2023-01-23 22:22:27 +0900

    Install Prettier into bloglist app (7.9)

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -1,97 +1,87 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
-import loginService from './services/login'
-import BlogForm from './components/BlogForm'
-import Togglable from './components/Togglable'
-import './App.css'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+import BlogForm from "./components/BlogForm";
+import Togglable from "./components/Togglable";
+import "./App.css";
 
-const TIMEOUT = 2500
+const TIMEOUT = 2500;
 
 const Notification = ({ message, state }) => {
   if (!message) {
-    return null
+    return null;
   }
 
-  return (
-    <div className={state}>
-      {message}
-    </div>
-  )
-}
+  return <div className={state}>{message}</div>;
+};
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [user, setUser] = useState(null)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
-  const [successMessage, setSuccessMessage] = useState('')
-
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
   // load all blogs
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs.sort((a, b) => b.likes - a.likes) )
-    )
-  }, [])
+    blogService
+      .getAll()
+      .then((blogs) => setBlogs(blogs.sort((a, b) => b.likes - a.likes)));
+  }, []);
 
   // load login information
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser')
+    const loggedUserJSON = window.localStorage.getItem("loggedBloglistUser");
     if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      console.log(`user ${user.username} has logged in`)
-      setUser(user)
-      blogService.setToken(user.token)
+      const user = JSON.parse(loggedUserJSON);
+      console.log(`user ${user.username} has logged in`);
+      setUser(user);
+      blogService.setToken(user.token);
     }
-  }, [])
-
+  }, []);
 
   const handleLogin = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     try {
-      const user = await loginService.login({ username, password })
+      const user = await loginService.login({ username, password });
 
-      blogService.setToken(user.token)
-      window.localStorage.setItem('loggedBloglistUser', JSON.stringify(user))
-      setUser(user)
-      console.log(`user ${username} has logged in`)
-      setUsername('')
-      setPassword('')
+      blogService.setToken(user.token);
+      window.localStorage.setItem("loggedBloglistUser", JSON.stringify(user));
+      setUser(user);
+      console.log(`user ${username} has logged in`);
+      setUsername("");
+      setPassword("");
     } catch (exception) {
-      setErrorMessage('username or password is incorrect')
-      setTimeout(() => setErrorMessage(''), TIMEOUT)
-      console.log(exception.message)
-      setPassword('')
+      setErrorMessage("username or password is incorrect");
+      setTimeout(() => setErrorMessage(""), TIMEOUT);
+      console.log(exception.message);
+      setPassword("");
     }
-  }
+  };
 
   const handleLogout = async () => {
-    window.localStorage.removeItem('loggedBloglistUser')
-    setUser(null)
-  }
+    window.localStorage.removeItem("loggedBloglistUser");
+    setUser(null);
+  };
 
   const handleCreate = async (blogObject) => {
     try {
+      const blog = await blogService.create(blogObject);
 
-      const blog = await blogService.create(blogObject)
+      console.log(`blog ${blog.title} has created`);
 
-      console.log(`blog ${blog.title} has created`)
+      setBlogs(blogs.concat(blog).sort((a, b) => b.likes - a.likes));
 
-      setBlogs(blogs.concat(blog).sort((a, b) => b.likes - a.likes))
-
-      setSuccessMessage(`a new blog ${blog.title} by ${blog.author} added`)
-      setTimeout(() => setSuccessMessage(''), TIMEOUT)
-
+      setSuccessMessage(`a new blog ${blog.title} by ${blog.author} added`);
+      setTimeout(() => setSuccessMessage(""), TIMEOUT);
     } catch (exception) {
-      setErrorMessage('something wrong with the new blog')
-      setTimeout(() => setErrorMessage(''), TIMEOUT)
-      console.log(exception.message)
+      setErrorMessage("something wrong with the new blog");
+      setTimeout(() => setErrorMessage(""), TIMEOUT);
+      console.log(exception.message);
     }
-  }
-
-
+  };
 
   const loginPage = () => (
     <div>
@@ -122,7 +112,7 @@ const App = () => {
         <button type="submit">login</button>
       </form>
     </div>
-  )
+  );
 
   const blogPage = () => (
     <div>
@@ -132,20 +122,16 @@ const App = () => {
       <p>
         {user.name} logged in<button onClick={handleLogout}>logout</button>
       </p>
-      <Togglable buttonLabel="new blog" >
+      <Togglable buttonLabel="new blog">
         <BlogForm handleCreate={handleCreate} />
       </Togglable>
-      {blogs.map(blog =>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} owned={user.id === blog.user.id} />
-      )}
+      ))}
     </div>
-  )
+  );
 
-  return (
-    user === null ?
-      loginPage() :
-      blogPage()
-  )
-}
+  return user === null ? loginPage() : blogPage();
+};
 
-export default App
+export default App;
