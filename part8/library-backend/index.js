90d4e064b4f87732f77d9a4dd38b5cf280073338
Haruta Tasaki <hrtune@gmail.com>
2023-01-28 22:58:59 +0900

    Implement addBook mutation (8.6)

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -104,6 +105,15 @@ const typeDefs = gql`
     allAuthors: [Author!]!
   }
 
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
+
   type Book {
     title: String!
     published: Int!
@@ -142,6 +152,27 @@ const resolvers = {
           bookCount,
         };
       }),
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      const newBook = {
+        ...args,
+        id: uuid(),
+      };
+      books = books.concat(newBook);
+
+      if (!authors.map((a) => a.name).includes(args.author)) {
+        const newAuthor = {
+          name: args.author,
+          id: uuid(),
+        };
+        authors = authors.concat(newAuthor);
+      }
+
+      console.log("book added:", newBook);
+      return newBook;
+    },
   },
 };
 
